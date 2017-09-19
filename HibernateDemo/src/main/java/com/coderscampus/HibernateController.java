beee535e4ea87d8fba80d0cf28dc3c2618ee5b51
DanielGS1572 <gonzalisd@gmail.com>
2017-09-18 23:46:09 -0500

    25.00

---
+++ HibernateDemo/src/main/java/com/coderscampus/HibernateController.java
@@ -0,0 +1,40 @@
+package com.coderscampus;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.coderscampus.domain.User;
+import com.coderscampus.repository.UserRepository;
+
+@Controller
+public class HibernateController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@RequestMapping(value = "user", method = RequestMethod.GET)
+	private @ResponseBody List<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+	@RequestMapping(value = "user", method = RequestMethod.POST)
+	private @ResponseBody User createUser(@RequestParam String userName, @RequestParam String password) {
+		User user = new User();
+		user.setUsername(userName);
+		user.setPassword(password);
+
+		user = userRepository.save(user);
+		return user;
+	}
+	
+	@RequestMapping(value="user/{id}", method= RequestMethod.DELETE)
+	private @ResponseBody void deleteUser(@RequestParam Long id){
+		userRepository.delete(id);
+	}
+}
