f30171f80697b3fdfb6bcfa810a2c46965ef6cc9
DanielGS1572 <gonzalisd@gmail.com>
2017-10-16 22:32:52 -0500

    jquery

--- CourseCreation/src/main/java/com/coderscampus/web/CourseController.java
+++ CourseCreation/src/main/java/com/coderscampus/web/CourseController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.coderscampus.domain.Course;
-import com.coderscampus.domain.Lesson;
 import com.coderscampus.repository.CourseRepository;
 
 @Controller
@@ -26,11 +25,19 @@ public class CourseController {
 	public String roothPath(){
 		return "redirect:courses";
 	}
-	@RequestMapping("courses")
-	public String courses(ModelMap model){
+	@RequestMapping(value= "courses", method=RequestMethod.GET)
+	public String coursePost(ModelMap model){
+	Course course = new Course();
 		List<Course> courses = courseRepository.findAll();
 		model.put("courses", courses);
-		return "courses";
+		model.put("course",	course);
+		return "courses";	
+	}
+	
+	@RequestMapping(value= "courses", method=RequestMethod.POST)
+	public String courses(@ModelAttribute Course course, ModelMap model){
+		courseRepository.save(course);
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value="editCourse/{courseId}",method=RequestMethod.GET)
@@ -41,36 +48,36 @@ public class CourseController {
 		return "editCourse";
 	}
 	
-	@RequestMapping(value="createCourse", method=RequestMethod.GET)
-	public String createCourseGet(ModelMap model){
-		Course course = new Course();
-		model.put("course", course);
-		return "createCourse";
-	}
+//	@RequestMapping(value="createCourse", method=RequestMethod.GET)
+//	public String createCourseGet(ModelMap model){
+//		Course course = new Course();
+//		model.put("course", course);
+//		return "createCourse";
+//	}
+//	
+//	@RequestMapping(value="createCourse", method=RequestMethod.POST)
+//	public String createCoursePost(@ModelAttribute Course course, ModelMap model){
+//		courseRepository.save(course);
+//		return "redirect:/";
+//	}
 	
-	@RequestMapping(value="createCourse", method=RequestMethod.POST)
-	public String createCoursePost(@ModelAttribute Course course, ModelMap model){
-		courseRepository.save(course);
-		return "redirect:/";
-	}
-	
-	@RequestMapping(value="editCourse/addLesson/{courseId}", method=RequestMethod.GET)
-	public String createLessonGet(@PathVariable Long courseId, ModelMap model){
-		Course course = courseRepository.findOne(courseId);
-		model.put("course", course);
-		
-		Lesson lesson = new Lesson();
-		
-		model.put("lesson", lesson);
-		return "addLesson";
-	}
-	@RequestMapping(value="editCourse/addLesson/{courseId}", method=RequestMethod.POST)
-	public String createLessonPost(@ModelAttribute Lesson lesson, @PathVariable Long courseId, ModelMap model){
-		Course course = courseRepository.findOne(courseId);
-		lesson.setCourse(course);
-		course.getLessons().add(lesson);
-		courseRepository.save(course);
-		return "redirect:/";
-		
-	}
+//	@RequestMapping(value="editCourse/addLesson/{courseId}", method=RequestMethod.GET)
+//	public String createLessonGet(@PathVariable Long courseId, ModelMap model){
+//		Course course = courseRepository.findOne(courseId);
+//		model.put("course", course);
+//		
+//		Lesson lesson = new Lesson();
+//		
+//		model.put("lesson", lesson);
+//		return "addLesson";
+//	}
+//	@RequestMapping(value="editCourse/addLesson/{courseId}", method=RequestMethod.POST)
+//	public String createLessonPost(@ModelAttribute Lesson lesson, @PathVariable Long courseId, ModelMap model){
+//		Course course = courseRepository.findOne(courseId);
+//		lesson.setCourse(course);
+//		course.getLessons().add(lesson);
+//		courseRepository.save(course);
+//		return "redirect:/";
+//		
+//	}
 }
