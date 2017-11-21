e4efe11de7beac9aa090ccd444d7c3e26e887840
DanielGS1572 <gonzalisd@gmail.com>
2017-11-20 21:43:03 -0600

    spring

---
+++ spudemy-spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		Coach theChoach = new TrackCoach();
+		System.out.println(theChoach.getDailyWorkout());
+	}
+
+}
