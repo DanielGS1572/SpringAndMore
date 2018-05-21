24bc2bd7777101bab77f401e3ae0e6106f58c0bb
DanielGS1572 <gonzalisd@gmail.com>
2018-05-20 23:18:18 -0500

    ocp

--- Generales/src/Polymorphism/App.java
+++ Generales/src/Polymorphism/App.java
@@ -1,6 +1,10 @@
 package Polymorphism;
 /*Lo mismo se puede hacer que las clases extiendan de Insect (ésta siendo una clase y no una interfaz)
  * pero se haria override al metodo*/
+
+//Nunca se tiene acceso como tal al objeto si no a la refrencia que apunta al objeto, y en realidad 
+//el objeto nunca cambia sus propiedades, si no se agregan nuevas dependiendo de la referencia que 
+//este apuntando a dicho objeto
 public class App {
 	public static void main(String args[]){
 		Insect[] listaInsectos = new Insect[3];   /*Ver como la referencia es la interfaz*/
@@ -12,6 +16,9 @@ public class App {
 		for(Insect a:listaInsectos){
 			System.out.println(a.eat());
 		}
-		
+		/*Castear un objeto de una Subclase a una superclase no necesita casteo*/
+		Object obj = new Beetle();
+		/*Castear un objeto de una superclase a una Subclase no necesita casteo*/
+		Beetle obj2 = (Beetle) new Object();
 	}
 }
