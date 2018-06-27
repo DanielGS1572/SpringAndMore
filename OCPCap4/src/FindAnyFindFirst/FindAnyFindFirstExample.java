e514d6bc094836d5c1b57c15d4063829c20c0878
dgonzali <dgonzali@legosoft.com.mx>
2018-06-27 18:56:06 -0500

    Katas

--- OCPCap4/src/FindAnyFindFirst/FindAnyFindFirstExample.java
+++ OCPCap4/src/FindAnyFindFirst/FindAnyFindFirstExample.java
@@ -13,7 +13,7 @@ public class FindAnyFindFirstExample {
 	public static void main(String[] args) {
 		Stream<String> s = Stream.of("monkey","gorilla","bonobo");
 		Stream<String> infinite = Stream.generate(()->"chimp");
-		s.findAny().ifPresent(System.out::println);
+		s.findAny().ifPresent(System.out::println);					
 		infinite.findAny().ifPresent(System.out::println);
 
 	}
