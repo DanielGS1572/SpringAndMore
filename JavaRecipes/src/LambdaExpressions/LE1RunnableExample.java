b1ce62029e1eb4c24ad61bba33b5f7b2ac17cc36
dgonzali <dgonzali@legosoft.com.mx>
2018-10-22 16:34:04 -0500

    lambda expression

--- JavaRecipes/src/LambdaExpressions/LE1RunnableExample.java
+++ JavaRecipes/src/LambdaExpressions/LE1RunnableExample.java
@@ -3,23 +3,24 @@ package LambdaExpressions;
 public class LE1RunnableExample {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+
+
 		new Thread(new Runnable() {					/*La clase Thread solo acepta una interfaz, en este caso se hace un Anonymous inner Class*/
-			
+
 			@Override
 			public void run() {
 				System.out.println("xxx");
-				
+
 			}
 		}).start();
-		
-		/*Runnable es una interfaz funcional, por lo que se puede aplicarle una expresión lambda*/
-		
-		
-		
-		
+
+		/*Runnable es una interfaz funcional (solo tiene un metodo que es run()), por lo que se puede aplicarle una expresión lambda*/
+		/*LAS EXPRESIONES LAMBDA SOOOOLO SE PUEDEN ASIGNAR A INTERFACES FUNCIONALES*/
+
+
+
+
 		/*Lo que importa es el tipo de retorno, como la interfaz funcional tiene un metodo que regresa void, y el System.out.println tambien regresa void
 		 * una expresión lambda puede ser asignada a la interfaz Runnable (también importa los argumentos que reciba), por ejemplo:*/
 		Runnable r = () -> System.out.println("xxx");				/*Ver como una expresion lambda es la definicion del cuerpo de una interfaz funcional,
