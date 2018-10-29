10582c47e59b58ec037576c5ac82ff8d152ee41b
DanielGS1572 <gonzalisd@gmail.com>
2018-10-28 22:22:57 -0600

    MReference

--- JavaRecipes/src/MethodReference/MR2Collect.java
+++ JavaRecipes/src/MethodReference/MR2Collect.java
@@ -3,6 +3,7 @@ package MethodReference;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MR2Collect {
 
@@ -10,6 +11,13 @@ public class MR2Collect {
 		List<String> strings = Arrays.asList("this","is","a","list");
 		List<String> sorted1 = strings.stream().sorted((s1,s2)-> s1.compareTo(s2)).collect(Collectors.toList());		/*Como sorted regresa un stream es necesario hacer un collect*/
 		List<String> sorted2 = strings.stream().sorted(String::compareTo).collect(Collectors.toList());					/*.sorted recibe un comparator por lo que ya se encarga de pasarle los parametros como van*/
+		
+		/*Contar cada palabra 
+		 * - Hecho con method reference*/
+		Stream.of("this","is","a","stream","of","String").map(String::length).forEach(System.out::println);				System.out.println();
+		
+		/* - Hecho con lambdas*/
+		Stream.of("this","is","a","stream","of","String").map(s -> s.length()).forEach(x -> System.out.println(x));
 	}
 
 }
