5230a1a79b2829889eb2be294836ef016b70467d
dgonzali <dgonzali@legosoft.com.mx>
2019-01-29 17:56:30 -0600

    angular

--- DEMOAngular/kendo/kendoFirstSteps/src/app/app.component.ts
+++ DEMOAngular/kendo/kendoFirstSteps/src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  
   title = 'kendoFirstSteps';
+  data: Array<any> = [
+    {
+      text:'Fun'
+    },{
+      text:'Funner'
+    },{
+      text:'Funnest'
+    }
+  ];
+
+  public data2: any[] = [
+    {
+      text: 'Furniture', items: [
+        { text: 'Tables & Chairs' },
+        { text: 'Sofas' },
+        {
+          text: 'Occasional Furniture', items: [{
+            text: 'Decor', items: [
+              { text: 'Bed Linen' },
+              { text: 'Curtains & Blinds' }
+            ]
+          }]
+        }
+      ]
+    },
+    { text: 'Decor' },
+    { text: 'Outdoors' }
+];
+public foods: Food[] = [
+  {value: 'steak-0', viewValue: 'Steak'},
+  {value: 'pizza-1', viewValue: 'Pizza'},
+  {value: 'tacos-2', viewValue: 'Tacos'}
+];
+
+public listItems: Array<string> = ["Baseball", "Basketball", "Cricket", "Field Hockey", "Football", "Table Tennis", "Tennis", "Volleyball"];
+
+  public onButtonItemClick(dataItem: any): void{
+    console.log(`You choose ${dataItem.text}`);
+  }
 }
--- DEMOAngular/kendo/kendoFirstSteps/src/app/app.module.ts
+++ DEMOAngular/kendo/kendoFirstSteps/src/app/app.module.ts
@@ -4,15 +4,43 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { MatCheckboxModule, MatMenuModule, MatButtonModule, MatIconModule,
+  MatDatepickerModule,        // <----- import(must)
+  MatNativeDateModule,        // <----- import for date formating(optional)
+  MatInputModule,MatSelectModule
+} from '@angular/material';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { TreeViewModule } from '@progress/kendo-angular-treeview';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
+    ButtonsModule,
+    InputsModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule, 
+    MatDatepickerModule,        // <----- import(must)
+    MatNativeDateModule,        // <----- import for date formating(optional)
+    MatInputModule, DateInputsModule, GridModule, TreeViewModule,MatSelectModule,DropDownsModule,FormsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
