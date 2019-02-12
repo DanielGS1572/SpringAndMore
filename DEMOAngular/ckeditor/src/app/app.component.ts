70d508fdd152bd9ba6274f8b572e685f2c6aa766
dgonzali <dgonzali@legosoft.com.mx>
2019-02-11 19:20:06 -0600

    angular

--- DEMOAngular/ckeditor/src/app/app.component.ts
+++ DEMOAngular/ckeditor/src/app/app.component.ts
@@ -16,12 +16,14 @@ export class AppComponent {
     let editor = this.ckEditor.instance;
     editor.config.height = '400';
     editor.config.toolbarGroups = [
-      { name:'document', groups: ['mode', 'document', 'doctools']},
+      /*{ name:'document', groups: ['mode', 'document', 'doctools']},
       { name:'clipboard', groups: ['clipboard','undo']},
       { name:'editing', groups: ['find','selection','spellchecker','editing']},
-      { name:'paragraph', groups: ['list','indent','blocks','align','bidi','paragraph']},
-      { name:'insert', groups: ['insert']}
+      { name:'paragraph', groups: ['list','indent','blocks','align','bidi','paragraph']},*/
+      { name:'insert', groups: ['insert','timestamp']}
     ]
+    //editor.config.extraPlugins = 'timestamp'
+    editor.config.extraPlugins = 'timestamp';
     editor.config.removeButton = 'Source, Save, Templates, Find, Replace, Scayt, SelectAll, Form, Radio';
   }
 }
