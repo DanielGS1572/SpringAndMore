c6f99613687143970326d98bddc8c6d1c48883a9
DanielGS1572 <gonzalisd@gmail.com>
2019-02-17 22:18:50 -0600

    angular

--- Angular/Udemy/Seccion 10/formularios/src/app/components/templates/templates.component.ts
+++ Angular/Udemy/Seccion 10/formularios/src/app/components/templates/templates.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-templates',
   templateUrl: './templates.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form){
+        border:1px solid red;
+    }
+  `]
 })
 export class TemplatesComponent implements OnInit {
-
+  usuario:Object={
+    nombre:null,
+    apellido:null,
+    correo: null
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+  guardar( forma:NgForm ){
+    console.log("formulario posteado");
+    console.log("ngForm", forma);
+    console.log("valor ", forma.value)      //trae todos los datos de la forma
+    console.log("usuario", this.usuario);
+    
   }
 
 }
