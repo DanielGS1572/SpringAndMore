9bed65ae12441c7de70e1355693621a400898924
DanielGS1572 <gonzalisd@gmail.com>
2019-02-19 09:44:04 -0600

    Angular

--- Angular/Udemy/Seccion 10/formularios/src/app/components/data/data.component.ts
+++ Angular/Udemy/Seccion 10/formularios/src/app/components/data/data.component.ts
@@ -8,18 +8,20 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';    //Requie
 export class DataComponent {
 
   forma:FormGroup;
-  usuario:Object={
+  usuario:any={
     nombreCompleto:{
       nombre:"fernando",
       apellido:"herrera"
     },correo:"correo@gmail.com"
   }
   constructor() { 
+    /** 4 Formas de setear la forma AAA) BBB) CCC) DDD) */
       console.log(this.usuario);
+      /**El objeto usuario que llene en la forma tiene la misma estructura */
       this.forma = new FormGroup({
 
         'nombreCompleto': new FormGroup({             /**Un formGroup dentro de otro FormGroup */
-           'nombre': new FormControl(''/*Nombre */, [
+           'nombre': new FormControl(''/*Nombre...this.usuario.nombreCompleto.nombre AAA) */, [
                                                   Validators.required,
                                                   Validators.minLength(3)
            ]   /**Regla de validacion ,Regla de validacion async */),
@@ -29,11 +31,24 @@ export class DataComponent {
                                       Validators.required, 
                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
       });
+     this.forma.setValue( this.usuario );    //como el objeto tiene la misma estructura que el form setea los campos en automatico  BBB)
 
   }
 
   guardarCambios(){
     console.log( this.forma.value );
     console.log( this.forma );
+
+    //PARA REGRESAR LA FORMA EN ESTADO PRISTINE SE USA RESET
+    //this.forma.reset( this.usuario );
+   this.forma.reset( {                  
+      nombreCompleto:{
+        nombre:"",
+        apellido: "" 
+      
+    },
+        correo:""
+    });                 /** DDD) --> ESTE SETEO LO DEJA EN ESTADO PRISTINE*/
+   /**this.forma.controls['correo'].setValue('nuevocorreo@gmail.com');            CCC)*/
   }
 }
