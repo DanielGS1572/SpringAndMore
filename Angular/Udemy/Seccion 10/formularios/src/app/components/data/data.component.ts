5356ad31718ab7e6915c80224eeaaff443d1d8f8
DanielGS1572 <gonzalisd@gmail.com>
2019-02-22 19:33:10 -0600

    Angular

--- Angular/Udemy/Seccion 10/formularios/src/app/components/data/data.component.ts
+++ Angular/Udemy/Seccion 10/formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';    //Requiere de ReactiveFormsModule en el app.module
+import { Observable } from 'rxjs';
+import { resolve } from 'q';
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
@@ -34,11 +36,31 @@ export class DataComponent {
                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
         'pasatiempos': new FormArray([
             new FormControl('', [Validators.required])      /**Si hubieran varios validadores va entre [] */
-        ])
+        ]),
+        'username': new FormControl('',Validators.required,this.existeUsuario),
+        'password1': new FormControl('',Validators.required),
+        'password2': new FormControl()
+
 
       });
      //this.forma.setValue( this.usuario );    //como el objeto tiene la misma estructura que el form setea los campos en automatico  BBB)
+      this.forma.controls['password2'].setValidators([
+        Validators.required,
+        this.noIgual.bind( this.forma )       //El bind hace que el this apunte a this.forma, se tiene que hacer esto porque el validador
+                //de no igual no esta en el mismo contexto
+      ]);
 
+      this.forma.valueChanges.subscribe( data =>{
+        console.log(data);
+      })            //escucha todos los cambios en los campos del formulario
+
+      this.forma.controls['username'].valueChanges.subscribe( data =>{
+        console.log(data);
+      })            //para escuchar cambios en un campo en particular
+
+      this.forma.controls['username'].statusChanges.subscribe( data =>{
+        console.log(data);
+      })            //para suscribirse a un estatus
   }
   agregarPasatiempo(){
     (<FormArray>this.forma.controls['pasatiempos']).push(new FormControl('',Validators.required));
@@ -53,6 +75,33 @@ export class DataComponent {
       }
       return null
   }
+
+  noIgual( control:FormControl ): {[s:string]:boolean} {    
+    let forma:any = this;      
+    if( control.value !== forma.controls['password1'].value){
+      return {
+        noiguales:true                
+      }
+    }
+    return null
+  }
+
+  existeUsuario( control: FormControl ) : Promise<any>|Observable<any>{
+    let promesa = new Promise(
+      (resolve, reject) =>
+      {
+        setTimeout(()=>{
+          if(control.value ==="strider"){
+            resolve({ existe: true})
+          }else{
+            resolve(null)
+          }
+        },3000) 
+      }
+    )
+    return promesa;
+  }
+
   guardarCambios(){
     console.log( this.forma.value );
     console.log( this.forma );
