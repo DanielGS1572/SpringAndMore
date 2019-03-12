bcbbd7a27371b50b8eec7cd7fddc9a0eb7583502
DanielGS1572 <gonzalisd@gmail.com>
2019-03-12 16:29:20 -0600

    CKEditor

--- DEMOAngular/ckeditor3/src/assets/ckeditor/plugins/valida/plugin.js
+++ DEMOAngular/ckeditor3/src/assets/ckeditor/plugins/valida/plugin.js
@@ -1,47 +1,43 @@
 CKEDITOR.plugins.add('valida', {
-	
-	init: function (editor) {
-		editor.addCommand( 'validaContenido', {
 
+	init: function (editor) {
+		editor.addCommand('validaContenido', {
 			// Define the function that will be fired when the command is executed.
-			exec: function( editor ) {
-				var diccionario = ["Germany","France","England","sit","amet"];
+			exec: function (editor) {
+				var diccionario = ["Germany", "France", "England", "sit", "amet"];
 				var valoresOk = [];
 				var cadena = editor.getData();
-				//var element = CKEDITOR.dom.element.createFromHtml( "<span style='background-color:cyan'> xxx </span>" );
-				//editor.insertElement( element );
-				
-													//editor.editable().setHtml("<span style='background-color:cyan!important'>ixxxpsum</span>");
 
-			/*	for(i=0; i < 10; i++){
-					var element = CKEDITOR.dom.element.createFromHtml("<span style='background-color:cyan'> " + i +  " </span>" );
-					editor.insertElement( element );
-				}*/
-					//editor.insertHtml("<h2>hey</h2>");
+				cadena = cadena.split("<span style=\"background-color:yellow!important\">").join("");
+				cadena = cadena.split("</span>").join("");
+				editor.editable().setHtml(cadena);
 
-				//editor.setHtml(editor.getData().replace("contenido","<span style='background-color:cyan'> xxx </span>"));
-				//editor.setHtml(writer);
-				var result = getFromBetween.get(editor.getData(),"[","]");
-console.log(result);
-diccionario.forEach(
-	(e1) =>result.forEach (
-		(e2)=>{
-		if(e1 === e2){
-			valoresOk.push(e1);
-			return;
-		}}));
+				var result = getFromBetween.get(editor.getData(), "[", "]");
 
-			result = result.filter( ( el ) => !valoresOk.includes( el ) );
-			for (var i = 0; i < result.length; i++) {
-				console.log(result[i]);
-				cadena = cadena.replace(result[i],"<span style='background-color:yellow!important'>"+result[i]+"</span>");
-			}
-			console.log(cadena);
-			editor.editable().setHtml(cadena);
+				diccionario.forEach(
+					(e1) => result.forEach(
+						(e2) => {
+							if (e1 === e2) {
+								valoresOk.push(e1);
+							}
+						}));
+				console.log(valoresOk);
+				console.log("valoresOk");
+
+				result = result.filter((el) => !valoresOk.includes(el));
+				console.log("valores NO ok");
+				console.log(result);
+
+				for (var i = 0; i < result.length; i++) {
+					cadena = cadena.replace(result[i], "<span style='background-color:yellow!important'>" + result[i] + "</span>");
+				}
+				console.log(cadena);
+				editor.editable().setHtml(cadena);
+
 			}
 		});
 
-	
+
 		editor.ui.addButton('validaContenido', {
 			label: 'Valida contenido',
 			command: 'validaContenido',
@@ -50,43 +46,45 @@ diccionario.forEach(
 
 		});
 
+
+
 		var getFromBetween = {
-			results:[],
-			string:"",
-			getFromBetween:function (sub1,sub2) {
-				if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
-				var SP = this.string.indexOf(sub1)+sub1.length;
-				var string1 = this.string.substr(0,SP);
+			results: [],
+			string: "",
+			getFromBetween: function (sub1, sub2) {
+				if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
+				var SP = this.string.indexOf(sub1) + sub1.length;
+				var string1 = this.string.substr(0, SP);
 				var string2 = this.string.substr(SP);
 				var TP = string1.length + string2.indexOf(sub2);
-				return this.string.substring(SP,TP);
+				return this.string.substring(SP, TP);
 			},
-			removeFromBetween:function (sub1,sub2) {
-				if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
-				var removal = sub1+this.getFromBetween(sub1,sub2)+sub2;
-				this.string = this.string.replace(removal,"");
+			removeFromBetween: function (sub1, sub2) {
+				if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
+				var removal = sub1 + this.getFromBetween(sub1, sub2) + sub2;
+				this.string = this.string.replace(removal, "");
 			},
-			getAllResults:function (sub1,sub2) {
+			getAllResults: function (sub1, sub2) {
 				// first check to see if we do have both substrings
-				if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return;
-		
+				if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return;
+
 				// find one result
-				var result = this.getFromBetween(sub1,sub2);
+				var result = this.getFromBetween(sub1, sub2);
 				// push it to the results array
 				this.results.push(result);
 				// remove the most recently found one from the string
-				this.removeFromBetween(sub1,sub2);
-		
+				this.removeFromBetween(sub1, sub2);
+
 				// if there's more substrings
-				if(this.string.indexOf(sub1) > -1 && this.string.indexOf(sub2) > -1) {
-					this.getAllResults(sub1,sub2);
+				if (this.string.indexOf(sub1) > -1 && this.string.indexOf(sub2) > -1) {
+					this.getAllResults(sub1, sub2);
 				}
 				else return;
 			},
-			get:function (string,sub1,sub2) {
+			get: function (string, sub1, sub2) {
 				this.results = [];
 				this.string = string;
-				this.getAllResults(sub1,sub2);
+				this.getAllResults(sub1, sub2);
 				return this.results;
 			}
 		};
